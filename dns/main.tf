a590c5ab7045e96a84132ef29933caf23cdaef27
LucasBello <lucas.bello@yahoo.com.br>
2020-07-07 02:22:13 -0300

    DNS

--- dns/main.tf
+++ dns/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "dns_cname_record" "mediawiki" {
+resource "dns_cname_record" "dns_wiki" {
   zone  = "vinnland.ml"
   name  = "mediawiki"
   cname = "MediaWikiELB-1429975875.us-west-2.elb.amazonaws.com"
